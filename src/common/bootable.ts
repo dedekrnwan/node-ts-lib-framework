88256ca45ae29df56efb9f3bfeaee44a506ced96
Dede Kurniawan <dekur98@outlook.com>
2020-01-18 00:22:05 +0700

    fixing bootable

--- src/common/bootable.ts
+++ src/common/bootable.ts
@@ -4,6 +4,7 @@ import {
 } from "./../interfaces";
 import fs from "fs";
 import { asyncForEach } from "../utils";
+import path from "path";
 
 export default class Bootable implements IApplicationUtils {
     public core:express.Application
@@ -13,11 +14,11 @@ export default class Bootable implements IApplicationUtils {
     use = (core: express.Application, app: Application): Promise<any> => new Promise<any>(async (resolve, reject) => {
         try {
             if(app.bootable) {
-                await asyncForEach(app.bootable, async(path) => {
-                    const dirList = fs.readdirSync(path);
+                await asyncForEach(app.bootable, async(folder) => {
+                    const dirList = fs.readdirSync(folder);
                     const promiseDirList = dirList.map((dirName) => new Promise<string>(async (res, rej) => {
                         try {
-                            const fName = path.join(path, dirName);
+                            const fName = path.join(folder, dirName);
                             const file = (require(fName).default) ? await require(fName).default() : fName;
                             res(file);
                         } catch (error) {
