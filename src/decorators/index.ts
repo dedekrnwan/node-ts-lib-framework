12b79f05ce7ccb69857e007e5a416fda3f374322
Dede Kurniawan <dekur98@outlook.com>
2020-01-17 23:44:04 +0700

    Revert "initial commit"
    This reverts commit a05848f6152fe6eff8758a0ecc548dc43627b270.

--- src/decorators/index.ts
+++
@@ -1,4 +0,0 @@
-export { AttachController } from "./attach";
-export { Controller } from "./controller"
-export { RouteMiddleware, ControllerMiddleware } from "./middleware"
-export { Route, Root } from "./route"
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import "reflect-metadata"
-import { App } from "./core";
-export * from "./decorators";
-export * from "./exception";
-export * from "./response";
-export * from "./core";
-
-export default App
